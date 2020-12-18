858e5f8929b50ae2ef92505dc60ef6ed7a4ac43b
Kay0s <hamilton.kristina@gmail.com>
2020-12-17 18:02:10 -0600

    updated package.json and / routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,6 +19,11 @@ module.exports = function (app) {
   });
 
   // If no matching route is found default to home
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  // If no matching route is found default to home
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
